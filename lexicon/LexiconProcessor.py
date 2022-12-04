89a858dac83db182d87779e2dd8b5d89d6025663
Alek Keersmaekers <u0111778@kuleuven.be>
2022-12-04 12:22:01 +0100

    Added extra function to LexiconProcessor

--- lexicon/LexiconProcessor.py
+++ lexicon/LexiconProcessor.py
@@ -1,8 +1,41 @@
-from bleach._vendor.html5lib.filters import lint
 class LexiconProcessor:
     
     def __init__(self,lexicon):
         self.lexicon = lexicon
+    
+    def add_data(self,data,feats,col_token=2,col_lemma=3,col_upos=4,col_xpos=5,col_morph=6):
+        for sent in data:
+            for line in sent.split("\n"):
+                split = line.split("\t")
+                morph = split[col_morph]
+                morph_dict = {}
+                if morph != '_':
+                    for feat_val in morph.split('|'):
+                        feat, val = feat_val.split('=')
+                        morph_dict[feat] = val
+                tag = []
+                for feat in feats:
+                    if feat == 'UPOS':
+                        tag.append((feat, split[col_upos]))
+                    elif feat == 'XPOS':
+                        tag.append((feat, split[col_xpos]))
+                    elif feat == 'lemma':
+                        tag.append((feat, split[col_lemma]))
+                    else:
+                        if feat in morph_dict:
+                            tag.append((feat, morph_dict[feat]))
+                        else:
+                            tag.append((feat, '_'))
+                tag = tuple(tag)
+                form = split[col_token]
+                if form in self.lexicon:
+                    tags = self.lexicon[form]
+                    if tag not in tags:
+                        tags.append(tag)
+                else:
+                    tags = []
+                    tags.append(tag)
+                    self.lexicon[form] = tags
     
     def write_lexicon(self,output,output_format,morph_feats,lemma_name='lemma',pos_name='XPOS'):
         entries_processed = set()
