931ae32d9883054ab0f4b9d883da47034dd3d883
alekkeersmaekers <37178560+alekkeersmaekers@users.noreply.github.com>
2024-10-06 21:34:56 +0200

    Added code to split datasets in n folds

--- data/Datasets.py
+++ data/Datasets.py
@@ -12,3 +12,20 @@ def build_dataset(tokens,tag_dict=None,wids=None):
                 sent_dict[tag_class] = tag_dict[tag_class][sent_id]
         dataset.append(sent_dict)
     return Dataset.from_list(dataset)        
+
+def split_n_fold(dataset,n_fold):
+    fold_size = int(len(dataset) / n_fold)
+    folds = []
+    index = 0
+    for i in range(n_fold):
+        data = {}
+        start = index
+        end = index + fold_size
+        if i+1 == n_fold or end > (len(dataset)):
+            end = len(dataset)
+        rows = range(start,end)
+        data['test'] = dataset.select(i for i in rows)
+        data['train'] = dataset.select(i for i in range(len(dataset)) if i not in rows)
+        folds.append(data)
+        index = index + fold_size
+    return folds
