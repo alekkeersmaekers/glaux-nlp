f816671c9e7cbe6e284142e3847891b81d4b712b
alekkeersmaekers <37178560+alekkeersmaekers@users.noreply.github.com>
2024-10-18 17:00:07 +0200

    Removed from superfluous line, and changed default quoting behavior

--- classification/TabularClassifier.py
+++ classification/TabularClassifier.py
@@ -6,10 +6,7 @@ class TabularClassifier:
     def __init__(self, features=None, td_file=None, td_format='tabular', test_file=None, test_format='tabular', transformer_embeddings_training=None, transformer_embeddings_test=None, normalize_columns=None, normalization='NFC', class_column=None):
         if td_file is not None:
             if td_format == 'tabular':
-                if class_column is not None:
-                    self.training_data = pd.read_csv(td_file, sep="\t", header=0, encoding="utf-8", quoting=3)
-                else:
-                    self.training_data = pd.read_csv(td_file, sep="\t", header=0, encoding="utf-8", quoting=3)
+                self.training_data = pd.read_csv(td_file, sep="\t", header=0, encoding="utf-8", quoting=0)
             if normalize_columns is not None:
                 for column in normalize_columns:
                     self.training_data = TabularDatasets.normalize_unicode(self.training_data, column, normalization)
@@ -19,10 +16,7 @@ class TabularClassifier:
                 self.training_data = self.training_data.astype({class_column: "category"})
         if test_file is not None:
             if test_format == 'tabular':
-                if class_column is not None:
-                    self.test_data = pd.read_csv(test_file, sep="\t", header=0, encoding="utf-8", quoting=3)
-                else:
-                    self.test_data = pd.read_csv(test_file, sep="\t", header=0, encoding="utf-8", quoting=3)
+                self.test_data = pd.read_csv(test_file, sep="\t", header=0, encoding="utf-8", quoting=0)
             if normalize_columns is not None:
                 for column in normalize_columns:
                     self.test_data = TabularDatasets.normalize_unicode(self.test_data, column, normalization)
